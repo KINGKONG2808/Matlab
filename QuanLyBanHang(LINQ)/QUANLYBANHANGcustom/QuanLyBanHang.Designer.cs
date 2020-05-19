9ba05dac8c3ee625a27d6be535053b2378b06681
HungVV <vuhung2881999@gmail.com>
2020-05-19 23:13:45 +0700

    update

---
+++ QuanLyBanHang(LINQ)/QUANLYBANHANGcustom/QuanLyBanHang.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace QUANLYBANHANGcustom
+{
+    partial class QuanLyBanHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodeProduct = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtNameProduct = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbKindOfProduct = new System.Windows.Forms.ComboBox();
+            this.ma_sp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten_sp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ma_loai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.don_gia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code Product";
+            // 
+            // txtCodeProduct
+            // 
+            this.txtCodeProduct.Location = new System.Drawing.Point(212, 37);
+            this.txtCodeProduct.Name = "txtCodeProduct";
+            this.txtCodeProduct.Size = new System.Drawing.Size(249, 22);
+            this.txtCodeProduct.TabIndex = 1;
+            this.txtCodeProduct.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name Product";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Unit Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Kind of Product";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(212, 205);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(249, 22);
+            this.txtUnitPrice.TabIndex = 5;
+            this.txtUnitPrice.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNameProduct
+            // 
+            this.txtNameProduct.Location = new System.Drawing.Point(212, 92);
+            this.txtNameProduct.Name = "txtNameProduct";
+            this.txtNameProduct.Size = new System.Drawing.Size(249, 22);
+            this.txtNameProduct.TabIndex = 7;
+            this.txtNameProduct.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(540, 37);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 33);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(540, 92);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(97, 33);
+            this.btnEdit.TabIndex = 9;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(540, 150);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 33);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(540, 205);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(97, 33);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ma_sp,
+            this.ten_sp,
+            this.ma_loai,
+            this.don_gia});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 279);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(652, 302);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // cbKindOfProduct
+            // 
+            this.cbKindOfProduct.FormattingEnabled = true;
+            this.cbKindOfProduct.Location = new System.Drawing.Point(212, 150);
+            this.cbKindOfProduct.Name = "cbKindOfProduct";
+            this.cbKindOfProduct.Size = new System.Drawing.Size(249, 24);
+            this.cbKindOfProduct.TabIndex = 13;
+            // 
+            // ma_sp
+            // 
+            this.ma_sp.DataPropertyName = "ma_sp";
+            this.ma_sp.HeaderText = "CodeProduct";
+            this.ma_sp.MinimumWidth = 6;
+            this.ma_sp.Name = "ma_sp";
+            this.ma_sp.Width = 125;
+            // 
+            // ten_sp
+            // 
+            this.ten_sp.DataPropertyName = "ten_sp";
+            this.ten_sp.HeaderText = "NameProduct";
+            this.ten_sp.MinimumWidth = 6;
+            this.ten_sp.Name = "ten_sp";
+            this.ten_sp.Width = 125;
+            // 
+            // ma_loai
+            // 
+            this.ma_loai.DataPropertyName = "ma_loai";
+            this.ma_loai.HeaderText = "KindOfProduct";
+            this.ma_loai.MinimumWidth = 6;
+            this.ma_loai.Name = "ma_loai";
+            this.ma_loai.Width = 125;
+            // 
+            // don_gia
+            // 
+            this.don_gia.DataPropertyName = "don_gia";
+            this.don_gia.HeaderText = "UnitPrices";
+            this.don_gia.MinimumWidth = 6;
+            this.don_gia.Name = "don_gia";
+            this.don_gia.Width = 125;
+            // 
+            // QuanLyBanHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 593);
+            this.Controls.Add(this.cbKindOfProduct);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNameProduct);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodeProduct);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyBanHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sale Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodeProduct;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtNameProduct;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbKindOfProduct;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma_sp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten_sp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma_loai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn don_gia;
+    }
+}
+
---
+++ QuanLyBanHang(LINQ)/QUANLYBANHANGcustom/QuanLyBanHang.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QUANLYBANHANGcustom
+{
+    public partial class QuanLyBanHang : Form
+    {
+        /*private String connectionString = @"Data Source=VUHUNGA49E;Initial Catalog=QUANLYBANHANGcustom;Integrated Security=True";
+        private SqlConnection connection = null;
+        private SqlDataAdapter dataAdapter = null;
+        private DataTable dataTable = null;
+        private SqlCommand cmd = null;*/
+
+
+        public QuanLyBanHang()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //connection = new SqlConnection(connectionString);
+            loadCombo();
+            loadData();
+        }
+
+        private void loadData()
+        {
+            /*dataAdapter = new SqlDataAdapter("select * from SanPham", connection);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;*/
+
+            dbDataContext data = new dbDataContext();
+            var sp = from sanPham in data.SanPhams select sanPham;
+            dataGridView1.DataSource = sp;
+        }
+        private void loadCombo()
+        {
+            /*dataAdapter = new SqlDataAdapter("Select * from LoaiSP", connection);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            cbKindOfProduct.DataSource = dataTable;
+            cbKindOfProduct.DisplayMember = "ten_loai";
+            cbKindOfProduct.ValueMember = "ma_loai";*/
+
+            dbDataContext data = new dbDataContext();
+            var lsp = from loaiSanPham in data.LoaiSPs select loaiSanPham;
+            cbKindOfProduct.DataSource = lsp;
+            cbKindOfProduct.DisplayMember = "ten_loai";
+            cbKindOfProduct.ValueMember = "ma_loai";
+        }
+        private void loadTimKiem(int loai)
+        {
+            /*dataAdapter = new SqlDataAdapter("select * from SanPham where ma_loai = " + loai, connection);
+            dataTable = new DataTable();
+            dataTable.Clear();
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;*/
+
+            dbDataContext data = new dbDataContext();
+            var search = from s in data.SanPhams where s.ma_loai == loai select new { s.ma_sp, s.ten_sp, s.ma_loai, s.don_gia };
+            dataGridView1.DataSource = search;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtCodeProduct.Text.Equals("") || txtUnitPrice.Text.Equals("") || txtNameProduct.Text.Equals(""))
+            {
+                MessageBox.Show("Not enough of data!!!");
+                return;
+            }
+            try
+            {
+                /*connection = new SqlConnection(connectionString);
+                connection.Open();
+                String sql = "exec nhap '" + txtCodeProduct.Text + "', '" + txtNameProduct.Text + "', " + Convert.ToInt32(cbKindOfProduct.SelectedValue) + "," + Convert.ToSingle(txtUnitPrice.Text);
+                cmd = new SqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                loadData();
+                MessageBox.Show("Add complete !!!");*/
+
+                dbDataContext data = new dbDataContext();
+                SanPham addProduct = new SanPham();
+                addProduct.ma_sp = txtCodeProduct.Text;
+                addProduct.ten_sp = txtNameProduct.Text;
+                addProduct.ma_loai = Convert.ToInt32(cbKindOfProduct.SelectedValue);
+                addProduct.don_gia = Convert.ToSingle(txtUnitPrice.Text);
+                data.SanPhams.InsertOnSubmit(addProduct);
+                data.SubmitChanges();
+                loadData();
+                MessageBox.Show("Add complete !!!");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Error!!!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure?", "Warning!!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+                return;
+            try
+            {
+                /*connection = new SqlConnection(connectionString);
+                connection.Open();
+                String sql = "Delete From SanPham where ma_sp = '" + txtCodeProduct.Text + "'";
+                cmd = new SqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                loadData();
+                MessageBox.Show("Delete complete !!!");*/
+
+                dbDataContext data = new dbDataContext();
+                var deleteProduct = from sp in data.SanPhams where sp.ma_sp == txtCodeProduct.Text select sp;
+                foreach(var i in deleteProduct)
+                {
+                    data.SanPhams.DeleteOnSubmit(i);
+                    data.SubmitChanges();
+                }
+                loadData();
+                MessageBox.Show("Delete complete !!!");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Error!!!");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*connection = new SqlConnection(connectionString);
+                connection.Open();
+                String sql = "Update SanPham set ten_sp = '" + txtNameProduct.Text + "', ma_loai = " + Convert.ToInt32(cbKindOfProduct.SelectedValue) + ", don_gia =" + Convert.ToSingle(txtUnitPrice.Text) + " where ma_sp ='" + txtCodeProduct.Text + "'";
+                MessageBox.Show(sql);
+                cmd = new SqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                loadData();
+                MessageBox.Show("Edit complete !!!");*/
+
+                dbDataContext data = new dbDataContext();
+                var updateProduct = data.SanPhams.Single(sp => sp.ma_sp == txtCodeProduct.Text);
+                updateProduct.ten_sp = txtNameProduct.Text;
+                updateProduct.ma_loai = Convert.ToInt32(cbKindOfProduct.SelectedValue);
+                updateProduct.don_gia = Convert.ToSingle(txtUnitPrice.Text);
+                data.SubmitChanges();
+                loadData();
+                MessageBox.Show("Edit complete !!!");
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Error!!!");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*connection = new SqlConnection(connectionString);
+                connection.Open();*/
+                loadTimKiem(Convert.ToInt32(cbKindOfProduct.SelectedValue));
+                if (dataGridView1.Rows.Count < 2)
+                {
+                    MessageBox.Show("The product is not exists !!!");
+                    return;
+                }
+                MessageBox.Show("Find all, see that !!!");
+
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Error!!!");
+            }
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            /*int row = e.RowIndex;
+            txtCodeProduct.Text = dataGridView1.Rows[row].Cells[0].Value.ToString();
+            txtNameProduct.Text = dataGridView1.Rows[row].Cells[1].Value.ToString();
+            cbKindOfProduct.SelectedValue = dataGridView1.Rows[row].Cells[2].Value.ToString();
+            txtUnitPrice.Text = dataGridView1.Rows[row].Cells[3].Value.ToString();*/
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txtCodeProduct.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtNameProduct.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            /* use for open connection normal
+            cbKindOfProduct.SelectedValue = dataGridView1.CurrentRow.Cells[2].Value.ToString();*/
+            // use for LINQ to SQL
+            cbKindOfProduct.SelectedValue = dataGridView1.CurrentRow.Cells[2].Value;
+            txtUnitPrice.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+    }
+}
