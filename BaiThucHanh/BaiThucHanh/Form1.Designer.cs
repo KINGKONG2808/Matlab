d8797017406f166d8146b1448374652a95a4a7c2
HungVV <vuhung2881999@gmail.com>
2020-08-11 00:41:36 +0700

    submit all

---
+++ BaiThucHanh/BaiThucHanh/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace BaiThucHanh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiemLan1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboMasv = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboMonHoc = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDiemLan2 = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.masv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.monhoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diemlan1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diemlan2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAP NHAT KET QUA HOC TAP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mon hoc";
+            // 
+            // txtDiemLan1
+            // 
+            this.txtDiemLan1.Location = new System.Drawing.Point(94, 159);
+            this.txtDiemLan1.Name = "txtDiemLan1";
+            this.txtDiemLan1.Size = new System.Drawing.Size(159, 20);
+            this.txtDiemLan1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ma sinh vien";
+            // 
+            // cboMasv
+            // 
+            this.cboMasv.FormattingEnabled = true;
+            this.cboMasv.Location = new System.Drawing.Point(94, 78);
+            this.cboMasv.Name = "cboMasv";
+            this.cboMasv.Size = new System.Drawing.Size(159, 21);
+            this.cboMasv.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Diem lan 1";
+            // 
+            // cboMonHoc
+            // 
+            this.cboMonHoc.FormattingEnabled = true;
+            this.cboMonHoc.Location = new System.Drawing.Point(363, 78);
+            this.cboMonHoc.Name = "cboMonHoc";
+            this.cboMonHoc.Size = new System.Drawing.Size(159, 21);
+            this.cboMonHoc.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Diem lan 2";
+            // 
+            // txtDiemLan2
+            // 
+            this.txtDiemLan2.Location = new System.Drawing.Point(363, 159);
+            this.txtDiemLan2.Name = "txtDiemLan2";
+            this.txtDiemLan2.Size = new System.Drawing.Size(159, 20);
+            this.txtDiemLan2.TabIndex = 7;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.masv,
+            this.hoten,
+            this.monhoc,
+            this.diemlan1,
+            this.diemlan2});
+            this.dataGridView.Location = new System.Drawing.Point(24, 306);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(498, 189);
+            this.dataGridView.TabIndex = 9;
+            this.dataGridView.Click += new System.EventHandler(this.dataGridView_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(57, 256);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Them";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(178, 256);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 11;
+            this.btnEdit.Text = "Sua";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(297, 256);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 12;
+            this.btnDel.Text = "Xoa";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(416, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Thoat";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Tim kiem";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(94, 210);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(231, 20);
+            this.txtSearch.TabIndex = 14;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(363, 210);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(159, 23);
+            this.btnSearch.TabIndex = 16;
+            this.btnSearch.Text = "Tim kiem";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // stt
+            // 
+            this.stt.DataPropertyName = "stt";
+            this.stt.HeaderText = "STT";
+            this.stt.Name = "stt";
+            // 
+            // masv
+            // 
+            this.masv.DataPropertyName = "masv";
+            this.masv.HeaderText = "MA SV";
+            this.masv.Name = "masv";
+            // 
+            // hoten
+            // 
+            this.hoten.DataPropertyName = "hoten";
+            this.hoten.HeaderText = "HO VA TEN";
+            this.hoten.Name = "hoten";
+            // 
+            // monhoc
+            // 
+            this.monhoc.DataPropertyName = "monhoc";
+            this.monhoc.HeaderText = "MON HOC";
+            this.monhoc.Name = "monhoc";
+            // 
+            // diemlan1
+            // 
+            this.diemlan1.DataPropertyName = "diemlan1";
+            this.diemlan1.HeaderText = "DIEM 1";
+            this.diemlan1.Name = "diemlan1";
+            // 
+            // diemlan2
+            // 
+            this.diemlan2.DataPropertyName = "diemlan2";
+            this.diemlan2.HeaderText = "DIEM 2";
+            this.diemlan2.Name = "diemlan2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Ho ten";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(94, 120);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(159, 20);
+            this.txtHoTen.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 507);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDiemLan2);
+            this.Controls.Add(this.cboMonHoc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cboMasv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDiemLan1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiemLan1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboMasv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboMonHoc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDiemLan2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn masv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn monhoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diemlan1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diemlan2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtHoTen;
+    }
+}
+
---
+++ BaiThucHanh/BaiThucHanh/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace BaiThucHanh
+{
+    public partial class Form1 : Form
+    {
+        public static int count = 0;
+        XmlDocument doc = new XmlDocument();
+        public static string filePath = "C:/Users/vuhung/source/repos/BaiThucHanh/BaiThucHanh/data.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataSet getData()
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(filePath);
+            return dataSet;
+        }
+
+        public void loadCombox()
+        {
+            cboMasv.DataSource = getData().Tables["sinhvien"];
+            cboMasv.DisplayMember = "masv";
+            cboMonHoc.DataSource = getData().Tables["sinhvien"];
+            cboMonHoc.DisplayMember = "monhoc";
+        }
+
+        public void loadData()
+        {
+            loadCombox();
+            dataGridView.DataSource = getData().Tables["sinhvien"];
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            doc.Load(filePath);
+            XmlElement stt = doc.CreateElement("stt");
+            stt.InnerText = (count+=1).ToString();
+            XmlElement hoTen = doc.CreateElement("hoten");
+            hoTen.InnerText = txtHoTen.Text;
+            XmlElement diemLan1 = doc.CreateElement("diemlan1");
+            diemLan1.InnerText = txtDiemLan1.Text;
+            XmlElement diemLan2 = doc.CreateElement("diemlan2");
+            diemLan2.InnerText = txtDiemLan2.Text;
+            XmlAttribute maSv = doc.CreateAttribute("masv");
+            maSv.InnerText = cboMasv.Text;
+            XmlAttribute monHoc = doc.CreateAttribute("monhoc");
+            monHoc.InnerText = cboMonHoc.Text;
+
+            XmlElement sinhVien = doc.CreateElement("sinhvien");
+            sinhVien.AppendChild(stt);
+            sinhVien.AppendChild(hoTen);
+            sinhVien.AppendChild(diemLan1);
+            sinhVien.AppendChild(diemLan2);
+            sinhVien.SetAttributeNode(maSv);
+            sinhVien.SetAttributeNode(monHoc);
+            doc.DocumentElement.AppendChild(sinhVien);
+            doc.Save(filePath);
+            loadData();
+            MessageBox.Show("Luu thanh cong");
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            doc.Load(filePath);
+            XmlNode xmlNode = doc.SelectSingleNode("/bangdiem/sinhvien[@masv='" + cboMasv.Text.Trim() + "'and @monhoc='" + cboMonHoc.Text.Trim() + "']");
+            if (xmlNode != null)
+            {
+                xmlNode.ChildNodes[0].InnerText = (count++).ToString();
+                xmlNode.ChildNodes[1].InnerText = txtHoTen.Text;
+                xmlNode.ChildNodes[2].InnerText = txtDiemLan1.Text;
+                xmlNode.ChildNodes[3].InnerText = txtDiemLan2.Text;
+                xmlNode.Attributes[1].InnerText = cboMonHoc.Text;
+                doc.Save(filePath);
+                loadData();
+                MessageBox.Show("Sua thanh cong");
+            } else
+            {
+                MessageBox.Show("Ma sinh vien khong ton tai");
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            doc.Load(filePath);
+            XmlNode xmlNode = doc.SelectSingleNode("/bangdiem/sinhvien[@masv='" + cboMasv.Text.Trim() + "'and @monhoc='" + cboMonHoc.Text.Trim() + "']");
+            if (xmlNode != null)
+            {
+                doc.DocumentElement.RemoveChild(xmlNode);
+                doc.Save(filePath);
+                loadData();
+                MessageBox.Show("Xoa thanh cong");
+            }
+            else
+            {
+                MessageBox.Show("Ma sinh vien khong ton tai");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Ban co chac muon thoat ?", "Canh bao", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.No)
+            {
+                return;
+            } else
+            {
+                this.Close();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void dataGridView_Click(object sender, EventArgs e)
+        {
+            txtHoTen.Text = dataGridView.CurrentRow.Cells[1].Value.ToString();
+            txtDiemLan1.Text = dataGridView.CurrentRow.Cells[2].Value.ToString();
+            txtDiemLan2.Text = dataGridView.CurrentRow.Cells[3].Value.ToString();
+            cboMasv.Text = dataGridView.CurrentRow.Cells[4].Value.ToString();
+            cboMonHoc.Text = dataGridView.CurrentRow.Cells[5].Value.ToString();
+        }
+    }
+}
